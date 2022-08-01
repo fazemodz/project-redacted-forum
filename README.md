05b1ca50286854b36ce8a4d4abf668932b37f35d
fazemodz <45793524+fazemodz@users.noreply.github.com>
2022-08-01 20:08:08 +0100

    added username checking

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## To do
 [] Add login system fully 
-[]Add .env to project so we can hide the api url in the code
+[] Add .env to project so we can hide the api url in the code
 [] Add email Verification
 [] Password Reset (through and email link?)
--- src/modules/login.tsx
+++ src/modules/login.tsx
@@ -1,61 +1,71 @@
-import { useState} from "react";
+import { useState } from "react";
 const Login: React.FC = () => {
 	const [Username, setUSername] = useState<string>('');
 	const [Password, setPassword] = useState<string>('');
 	const [IsUsernameInvalid, setIsUsernameInvalid] = useState<boolean>(false);
 	const onSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
-		if(Username.includes('@') && Username.includes('.')) {
+			event.preventDefault();
+			console.log(Username, Password);
+	}
+	const CheckUsername = () => {
+		if (Username.includes('@') && Username.includes('.')) {
 			setIsUsernameInvalid(true);
-			console.log('Username is not valid');
 		}
 		else {
-		event.preventDefault();
-		console.log(Username, Password);
+			setIsUsernameInvalid(false);
 		}
 	}
 	return (
-		
+
 		<div className="bg-grey-lighter min-h-screen flex flex columns">
-		<div className="container max-w-sm mx-auto flex 1 flex-col items-center justify-center px-2">
-		  <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-			<h1 className="mb-8 text-3x1 text-center">Login</h1>
-			<input
-			  type="Username"
-			  placeholder="Username"
-			  value={Username}
-			  onChange={(e) => setUSername(e.target.value)}
-			  id="username"
-			  className="block border border-grey-light w-full p-3 rounded mb-4"
-			/>
-			<label
-			  htmlFor="username"
-			  className={IsUsernameInvalid ? "block text-red-500" : "block text-grey-darker"}
-			/>
-			<input
-			  type="password"
-			  placeholder="Password"
-			  value={Password}
-			  onChange={(e) => setPassword(e.target.value)}
-			  id="password"
-			  className="block border border-grey-light w-full p-3 rounded mb-4"
-			/>
-			<button 
-			type="submit" 
-			className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline" 
-			onClick={onSubmit}
-			>
-			  Login
-			</button>
-		  </div>
-		  <div className="text-grey-dark mt-6">
-			Don't have an account?
-			<a className="no-underline border-b border-blue text-blue" href="/register">
-			  Register here
-			</a>.
-		  </div>
+			<div className="container max-w-sm mx-auto flex 1 flex-col items-center justify-center px-2">
+				<div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+					<h1 className="mb-8 text-3x1 text-center">Login</h1>
+					<input
+						type="Username"
+						placeholder="Username"
+						value={Username}
+						onChange={(e) => setUSername(e.target.value)}
+						onBlur={CheckUsername}
+						id="username"
+						className="block border border-grey-light w-full p-3 rounded mb-4"
+					/>
+					{
+						IsUsernameInvalid ?
+						<label
+							htmlFor="username"
+							className="block text-red-500"
+						>
+							Username is invalid
+						</label>
+						:
+						null
+					}
+					<input
+						type="password"
+						placeholder="Password"
+						value={Password}
+						onChange={(e) => setPassword(e.target.value)}
+						id="password"
+						className="block border border-grey-light w-full p-3 rounded mb-4"
+					/>
+					<button
+						type="submit"
+						className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+						onClick={onSubmit}
+					>
+						Login
+					</button>
+				</div>
+				<div className="text-grey-dark mt-6">
+					Don't have an account?
+					<a className="no-underline border-b border-blue text-blue" href="/register">
+						Register here
+					</a>.
+				</div>
+			</div>
+
 		</div>
-  
-	  </div>
 	);
 }
 export default Login;
