d545dacbd7c32a85a72d9cdbc06081d1b2ca6674
fazemodz <hike820@gmail.com>
2022-08-03 06:00:34 +0100

    lol

--- src/modules/login.tsx
+++ src/modules/login.tsx
@@ -1,5 +1,5 @@
 import axios, { AxiosError } from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Login: React.FC = () => {
 	const [Username, setUSername] = useState<string>('');
 	const [Password, setPassword] = useState<string>('');
@@ -20,7 +20,15 @@ const Login: React.FC = () => {
 					console.log("An error occurred: " + reaseon.message);
 				});
 	}
-	const CheckUsername = () => {
+	const [UserPrefersDarkmode , setUserPrefersDarkmode] = useState<boolean>(false);
+	useEffect(() => {
+		if(localStorage.getItem('DarkmodeEnabeled') === 'true') {
+			setUserPrefersDarkmode(true);
+		}else {
+			setUserPrefersDarkmode(false);
+		}
+	} , [])
+	const CheckUsername =	 () => {
 		if (Username.includes('@') && Username.includes('.')) {
 			setIsUsernameInvalid(true);
 		}
@@ -29,7 +37,60 @@ const Login: React.FC = () => {
 		}
 	}
 	return (
+		<>
+		{
+			UserPrefersDarkmode ?
+			//to do: add darkmode ver of form 
+			<div className="bg-grey-lighter min-h-screen flex flex columns">
+			<div className="container max-w-sm mx-auto flex 1 flex-col items-center justify-center px-2">
+				<div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+					<h1 className="mb-8 text-3x1 text-center">Login</h1>
+					<input
+						type="Username"
+						placeholder="Username"
+						value={Username}
+						onChange={(e) => setUSername(e.target.value)}
+						onBlur={CheckUsername}
+						id="username"
+						className="block border border-grey-light w-full p-3 rounded mb-4"
+					/>
+					{
+						IsUsernameInvalid ?
+						<label
+							htmlFor="username"
+							className="block text-red-500"
+						>
+							Username is invalid
+						</label>
+						:
+						null
+					}
+					<input
+						type="password"
+						placeholder="Password"
+						value={Password}
+						onChange={(e) => setPassword(e.target.value)}
+						id="password"
+						className="block border border-grey-light w-full p-3 rounded mb-4"
+					/>
+					<button
+						type="submit"
+						className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+						onClick={onSubmit}
+					>
+						Login
+					</button>
+				</div>
+				<div className="text-grey-dark mt-6">
+					Don't have an account?
+					<a className="no-underline border-b border-blue text-blue" href="/register">
+						Register here
+					</a>.
+				</div>
+			</div>
 
+		</div>
+		:
 		<div className="bg-grey-lighter min-h-screen flex flex columns">
 			<div className="container max-w-sm mx-auto flex 1 flex-col items-center justify-center px-2">
 				<div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
@@ -79,6 +140,8 @@ const Login: React.FC = () => {
 			</div>
 
 		</div>
+		}
+		</>
 	);
 }
 export default Login;
