895e63b7a50c07fd4c0301f7dde9dd8cee447f13
fazemodz <hike820@gmail.com>
2023-03-28 10:33:47 +0100

    test

--- src/modules/Navbar.tsx
+++ src/modules/Navbar.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import'./resources/Header.css'
 import {isuserLoggedin} from "./utils/NeededInformation";
 const Navbar: React.FC = () => {
     React.useEffect(() => {
        console.log(isuserLoggedin);
     }, [])
+    const checkuser = () => {
+        if(isuserLoggedin !== 'true'){
+            alert("user not logged in")
+        }else{
+            alert("user logged in")
+        }
+    }
+    
     return (
         <div className="Navbar-container">
             <div className="Navbar-section logo">
